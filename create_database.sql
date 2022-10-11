29413f02c68cf341430c750bb7e554b91c0677ee
Cristian Iordachescu <iordachescu1996@outlook.com>
2022-10-11 22:04:33 +0300

    corected database name

--- create_database.sql
+++ create_database.sql
@@ -2,5 +2,5 @@
 
 CREATE USER 'senzorserverapp'@'%' IDENTIFIED BY 'SenzorServerApp2022';
 GRANT USAGE ON *.* TO 'senzorserverapp'@'%' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
-CREATE DATABASE IF NOT EXISTS `senzorserverapp2`;
-GRANT ALL PRIVILEGES ON `senzorserverapp2`.* TO 'senzorserverapp'@'%';
+CREATE DATABASE IF NOT EXISTS `senzorserverapp`;
+GRANT ALL PRIVILEGES ON `senzorserverapp`.* TO 'senzorserverapp'@'%';
